1249dbaff10980818c702b29d9accb15110b37b7
Yuriman123 <76440720+Yuriman123@users.noreply.github.com>
2021-04-17 15:10:49 +0900

    カテゴリーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,19 +9,19 @@ class ItemsController < ApplicationController
   @item = Item.new
   end
 
-  # def create
-  #  @item = Item.new(item_params)
-  #  if @item.save  # create = new + save   Item.createは、@item = Item.new(item_params) →@item.saveと同義
-  #   redirect_to controller: :items, action: :index
-  #  else
-  #   render "new"
-  #  end
-  # end
+  def create
+   @item = Item.new(item_params)
+   if @item.save  # create = new + save   Item.createは、@item = Item.new(item_params) →@item.saveと同義
+    redirect_to controller: :items, action: :index
+   else
+    render "new"
+   end
+  end
   
   private
-  # def item_parmas
-  #   params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
-  # end
+  def item_parmas
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
 
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,22 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :area
-
-  has_one_attached :image
+    belongs_to :area,:category,:status,:elivery_burden,:shipping_day
+    has_one_attached :image
 
   with_options presence: true do
 
-    validates :nickname
+    validates :name
     validates :explanation
-    validates :category_id
-    validates :status_id
-    validates :delivery_burden_id
-    validates :shipping_day_id
-    validates :price
+
+    validates :image,              numericality: { other_than: 1 } 
+    validates :category_id,        numericality: { other_than: 1 } 
+    validates :status_id,          numericality: { other_than: 1 } 
+    validates :delivery_burden_id, numericality: { other_than: 1 } 
+    validates :shipping_day_id,    numericality: { other_than: 1 } 
+    validates :area_id,            numericality: { other_than: 1 } 
+
+    validates :price, inclusion: { in: 300..9_999_999 },format: { with: /\A[0-9]+\z/} 
   end
-    validates :area_id, numericality: { other_than: 1 } 
-    # validates :user
 
 
 end
