9a0d16a23de768be8de441ae0013b7387097b9ba
Yuriman123 <76440720+Yuriman123@users.noreply.github.com>
2021-04-19 21:15:56 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,33 +1,31 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,except:[:index]
-
+  before_action :authenticate_user!, except: [:index]
 
   def index
     @items = Item.includes(:user)
   end
 
   def new
-  @item = Item.new
+    @item = Item.new
   end
 
   def create
     @item = Item.new(item_params)
     if @item.save  # create = new + save   Item.createは、@item = Item.new(item_params) →@item.saveと同義
-     redirect_to root_path
-   else
-     render "new"
-   end
+      redirect_to root_path
+    else
+      render 'new'
+    end
 
-  def show
-    @item = Item.find(item_params[:id])
-  end 
-
+    def show
+      @item = Item.find(item_params[:id])
+    end
   end
-  
+
   private
+
   def item_params
-    params.require(:item).permit(:name,:explanation,:price,:image,:area_id,:category_id,:status_id, :delivery_burden_id,:shipping_day_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :explanation, :price, :image, :area_id, :category_id, :status_id, :delivery_burden_id,
+                                 :shipping_day_id).merge(user_id: current_user.id)
   end
-
-
 end
--- app/models/shipping_day.rb
+++ app/models/shipping_day.rb
@@ -1,11 +1,9 @@
 class ShippingDay < ActiveHash::Base
-
   self.data = [
-    {id: 1, name: '--'},
-    {id: 2, name: '1日〜２日で発送'}, {id: 3, name: '２日〜３日で発送'}, {id: 4, name: '４日〜７日で発送'},
-   ]
+    { id: 1, name: '--' },
+    { id: 2, name: '1日〜２日で発送' }, { id: 3, name: '２日〜３日で発送' }, { id: 4, name: '４日〜７日で発送' }
+  ]
 
- include ActiveHash::Associations
- has_many :items
-
+  include ActiveHash::Associations
+  has_many :items
 end
