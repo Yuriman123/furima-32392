4e49aaf40c5b3b3a0be54eb67998721946ca6b05
Yuriman123 <76440720+Yuriman123@users.noreply.github.com>
2021-04-19 21:16:15 +0900

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:firstname,:familyname,:firstname_kana,:familyname_kana,:birthday])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :firstname, :familyname, :firstname_kana, :familyname_kana, :birthday])
   end
 end
