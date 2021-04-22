56f69c0335220b4af524a649253e026b99bbbb6d
Yuriman123 <76440720+Yuriman123@users.noreply.github.com>
2021-04-22 21:00:11 +0900

    リダイレクト機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,9 +26,8 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+   unless current_user == @item.user_id
     redirect_to root_path
-   unless
-    current_user.id == @item.user_id
    end
   end
 
