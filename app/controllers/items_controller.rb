012a7399bd2f23a4e0a2a57dbeb546d045ce292e
Yuriman123 <76440720+Yuriman123@users.noreply.github.com>
2021-04-29 16:36:58 +0900

    修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :update, :show, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update, :destroy]
-  # before_action :redirect_to, only:[:edit]
 
 
   def index
@@ -26,14 +25,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.order.presence
-      redirect_to root_path
-    end
   end
 
   def update
     if @item.update(item_params)
-      @item.order.presence
       redirect_to root_path
     else
       render :edit
@@ -57,7 +52,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if current_user.id != @item.user.id
+    redirect_to root_path if current_user.id != @item.user.id ||  @item.order.presence
   end
 
 end
