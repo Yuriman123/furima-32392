b2c4544ab33069174465b2465cfdfb816838aecb
Yuriman123 <76440720+Yuriman123@users.noreply.github.com>
2021-04-29 15:22:31 +0900

    Revert "Revert "Merge pull request #9 from Yuriman123/商品購入機能""
    This reverts commit 545c3c4dc29ff63404bdbaba7177d6dae2495cab.

---
+++ app/models/user_order.rb
@@ -0,0 +1,22 @@
+class UserOrder
+  include ActiveModel::Model
+  attr_accessor :postno, :area_id, :address, :street, :building, :tell,:image, :building, :item_id,:user_id,:token
+
+  with_options presence: true do
+    validates :postno, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :area_id, numericality: { other_than: 1 } 
+    validates :address, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    validates :street
+    validates :tell, format: { with: /\A\d{10,11}\z/ }
+    validates :token
+    validates :user_id
+    validates :item_id
+  end
+  
+ def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    mailingaddress = Mailingaddress.create(postno: postno, area_id: area_id, address: address, street: street, building: building, tell: tell, order_id: order.id)
+ end
+
+
+end
